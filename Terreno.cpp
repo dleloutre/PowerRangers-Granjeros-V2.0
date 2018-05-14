78b05ce69c59b1fb677931274ce9ec91e426d5bc
dleloutre <39065230+dleloutre@users.noreply.github.com>
2018-05-14 19:19:37 -0300

    update
    breve ej.:
    terreno.modificarTerreno(1,1)->regarParcela(); // riega parcela, cambia el booleano 'estadoRiego' a true
    cout << terreno.obtenerTerreno(1,1).regado() << endl; // imprime 1
    cout << terreno.obtenerTerreno(2,1).regado() << endl; // imprime 0 porque no no lo regué

--- Terreno.cpp
+++ Terreno.cpp
@@ -3,6 +3,7 @@
 Terreno::Terreno(){
 	Parcela crearParcela;
 	this->tamanio = TAMANIO_MINIMO;
+	this->accesoATerreno = NULL;
 	for (unsigned int i=0; i<this->tamanio; i++) {
 		for (unsigned int j=0; j<this->tamanio; j++) {
 			terreno[i][j]=crearParcela;
@@ -13,6 +14,7 @@ Terreno::Terreno(){
 Terreno::Terreno(unsigned int tamanio){
 	Parcela crearParcela;
 	this->tamanio = tamanio;
+	this->accesoATerreno = NULL;
 	for (unsigned int i=0; i<this->tamanio; i++) {
 		for (unsigned int j=0; j<this->tamanio; j++) {
 			terreno[i][j]=crearParcela;
@@ -22,6 +24,11 @@ Terreno::Terreno(unsigned int tamanio){
 
 Parcela Terreno::obtenerTerreno(unsigned int posicionFila, unsigned int posicionColumna){
 	return this->terreno[posicionFila][posicionColumna];
+}
+
+Parcela* Terreno::modificarTerreno(unsigned int posicionFila, unsigned int posicionColumna){
+	this->accesoATerreno = &this->terreno[posicionFila][posicionColumna];
+	return this->accesoATerreno;
 }
 
 unsigned int Terreno::obtenerTamanio(){
