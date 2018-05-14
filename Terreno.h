94a83dd7104519dfb31db00bb1e540056a4f3ed7
dleloutre <39065230+dleloutre@users.noreply.github.com>
2018-05-14 19:15:46 -0300

    update

--- Terreno.h
+++ Terreno.h
@@ -9,6 +9,7 @@ class Terreno {
 	private:
 		unsigned int tamanio;
 		Parcela terreno[TAMANIO_MINIMO][TAMANIO_MINIMO];
+		Parcela* accesoATerreno;
 
 	public:
 
@@ -18,10 +19,12 @@ class Terreno {
 		//POST: crea la matriz 'terreno' de tamanio filas y tamanio columnas
 		Terreno(unsigned int tamanio);
 
-
 		//POST: devuelve la parcela que se encuentra en la posicionFila y posicionColumna del terreno
 		Parcela obtenerTerreno(unsigned int posicionFila, unsigned int posicionColumna);
 
+		//POST: accede a la parcela que se encuentra en la posicionFila y posicionColumna del terreno
+		Parcela* modificarTerreno(unsigned int posicionFila, unsigned int posicionColumna);
+	
 		//POST: devuelve la cantidad de filas y columnas de la matriz 'terreno'
 		unsigned int obtenerTamanio();
 
