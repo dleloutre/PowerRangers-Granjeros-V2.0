ac822899e89755ef73c9be1ccc91b73692db275c
dleloutre <39065230+dleloutre@users.noreply.github.com>
2018-05-16 15:00:22 -0300

    Update Terreno.cpp

--- Terreno.cpp
+++ Terreno.cpp
@@ -4,33 +4,48 @@ Terreno::Terreno(){
 	Parcela crearParcela;
 	this->tamanio = TAMANIO_MINIMO;
 	this->accesoATerreno = NULL;
+	this->terreno = new Parcela* [this->tamanio];
+	for (unsigned int a=0; a < this->tamanio; a++){
+			this->terreno[a] = new Parcela[this->tamanio];
+		}
 	for (unsigned int i=0; i<this->tamanio; i++) {
 		for (unsigned int j=0; j<this->tamanio; j++) {
-			terreno[i][j]=crearParcela;
+			terreno[i][j] = crearParcela;
 		}
 	}
 }
 
 Terreno::Terreno(unsigned int tamanio){
 	Parcela crearParcela;
-	this->tamanio = tamanio;
 	this->accesoATerreno = NULL;
+	this->tamanio = tamanio;
+	this->terreno = new Parcela* [this->tamanio];
+	for (unsigned int a=0; a < this->tamanio; a++){
+			this->terreno[a] = new Parcela[this->tamanio];
+		}
+
 	for (unsigned int i=0; i<this->tamanio; i++) {
 		for (unsigned int j=0; j<this->tamanio; j++) {
-			terreno[i][j]=crearParcela;
+			terreno[i][j] = crearParcela;
 		}
 	}
 }
 
-Parcela Terreno::obtenerTerreno(unsigned int posicionFila, unsigned int posicionColumna){
-	return this->terreno[posicionFila][posicionColumna];
+Parcela Terreno::obtenerParcela(unsigned int posicionFila, unsigned int posicionColumna){
+	return this->terreno[posicionFila-1][posicionColumna-1];
 }
 
 Parcela* Terreno::modificarTerreno(unsigned int posicionFila, unsigned int posicionColumna){
-	this->accesoATerreno = &this->terreno[posicionFila][posicionColumna];
+	this->accesoATerreno = &this->terreno[posicionFila-1][posicionColumna-1];
 	return this->accesoATerreno;
 }
 
 unsigned int Terreno::obtenerTamanio(){
 	return this->tamanio;
+}
+
+Terreno::~Terreno(){
+	for(unsigned int i = 0; i < this->tamanio; i++)
+	   delete [] this->terreno[i];
+	delete [] this->terreno;
 }
