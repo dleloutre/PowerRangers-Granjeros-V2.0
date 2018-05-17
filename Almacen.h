45a819e859ac9d085083e1919cb610e258f35260
dleloutre <39065230+dleloutre@users.noreply.github.com>
2018-05-17 13:05:47 -0300

    Update Almacen.h

--- Almacen.h
+++ Almacen.h
@@ -7,6 +7,7 @@
 class Almacen
 {
 private:
+
 	Lista<Cosecha*>* bodega;
 	unsigned int capacidadTotal;
 	unsigned int capacidadDisponible;
@@ -14,20 +15,25 @@ private:
 	unsigned int capacidadDeUnAlmacen;
 
 public:
+
 	Almacen(int filas, int columnas, int dificultad, Lista<Cosecha*>* bodega);
+
 	//pre:
 	//post: devuelve la capacidad total del almacen
 	unsigned int obtenerCapacidadTotal();
+
 	//pre
 	//post: devuelve la capacidad disponible en el almacen
 	unsigned int obtenerCapacidadDisponible();
+
 	//pre
 	//post:devuelve la capacidad ocupada del almacen
-
 	unsigned int obtenerCapacidadOcupada();
+
 	//pre:
 	//post añade capacidad equivalente a un almacen a la capacidad total
 	void agregarCapacidadTotal();
+
 	//pre: hay lugar disponible
 	//post: ocupa un lugar en el almacen
 	void ocuparUnLugar();
@@ -44,6 +50,7 @@ public:
 	//post: si el tipo de cosecha ya se encuentra en el almacen se le suma uno a su cantidad, si no, se agrega el tipo
 	//      de cosecha a la lista y su cantidad inicia en 1
 	int agregarUnaCosecha(char tipoCosecha);
+
 	//pre:
 	//post: devuelve la cantidad que hay  del tipo de cosecha ingresado en el almacen.
 	int obtenerCantidadCosecha(char tipoCosecha);
