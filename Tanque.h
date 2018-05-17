f6fe0307c8617d236f7fca8e43c8b297452a9d41
dleloutre <39065230+dleloutre@users.noreply.github.com>
2018-05-16 23:39:30 -0300

    Create Tanque.h

---
+++ Tanque.h
@@ -0,0 +1,58 @@
+#ifndef TANQUE_H_
+#define TANQUE_H_
+
+
+#include "CatalogoCultivos.h"
+
+
+class Tanque
+{
+private:
+
+	int capacidadTotal;
+	int capacidadOcupada;
+	int capacidadLibre;
+	int aguaDisponiblePorEsteTurno;
+public:
+	Tanque(int n, int m, float dificultad);
+
+	//pre:
+	//post: devuelve la capacidad total del tanque
+
+	int obtenerCapacidadTotal();
+
+	// pre:
+	// post: devuelve la cantidad de unidades de agua que tiene el tanque
+
+	int obtenerCapacidadOcupada();
+	// pre:
+	//post devuelve la cantidad de espacio libre en el tanque
+
+	int obtenerCapacidadLibre();
+
+
+
+	//pre: el cultivo está en la lista de cultivos y el agua disponible en el tanque es mayor a las unidades de riego
+	//     necesarias para el cultivo.
+
+	//post: utiliza las unidades de agua requeridas
+
+	int utilizarAgua(Lista<Cultivo*>* cultivos, char cultivoParcela);
+
+	//pre:
+	//post: establece la cantidad de agua disponible por este turno, se calcula mediante la suma del
+	//      agua por turno (que es aleatoria) más la capacidad Ocupada del tanque.
+
+	int determinarAguaPorEsteTurno();
+
+	//pre:
+	//post:  actualiza la capacidad Ocupada y capacidad Disponible.
+	void actualizarCapacidades();
+
+	//pre:
+	//post: devuelve la cantdiad de agua que se puede utilizar.
+	int obtenerAguaDisponible();
+};
+
+
+#endif /* TANQUE_H_ */
