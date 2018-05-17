c941a78637c4ecf847dcd5d7e457c6e6110e6251
dleloutre <39065230+dleloutre@users.noreply.github.com>
2018-05-17 13:19:52 -0300

    Update Tanque.h

--- Tanque.h
+++ Tanque.h
@@ -2,17 +2,17 @@
 #define TANQUE_H_
 
 
-#include "CatalogoCultivos.h"
+#include "CatalogoSemillas.h"
 
 
 class Tanque
 {
 private:
-
 	int capacidadTotal;
 	int capacidadOcupada;
 	int capacidadLibre;
 	int aguaDisponiblePorEsteTurno;
+
 public:
 	Tanque(int n, int m, float dificultad);
 
@@ -30,14 +30,12 @@ public:
 
 	int obtenerCapacidadLibre();
 
-
-
-	//pre: el cultivo está en la lista de cultivos y el agua disponible en el tanque es mayor a las unidades de riego
+	//pre: 'nombreSemillaSeleccionada' está en la lista de semillas y el agua disponible en el tanque es mayor a las unidades de riego
 	//     necesarias para el cultivo.
 
 	//post: utiliza las unidades de agua requeridas
 
-	int utilizarAgua(Lista<Cultivo*>* cultivos, char cultivoParcela);
+	int utilizarAgua(Lista<Semilla*>* semillas, char nombreSemillaSeleccionada);
 
 	//pre:
 	//post: establece la cantidad de agua disponible por este turno, se calcula mediante la suma del
