7c90064de5453885849b6259c8ef74675b127835
dleloutre <39065230+dleloutre@users.noreply.github.com>
2018-05-17 13:20:27 -0300

    Update Tanque.cpp

--- Tanque.cpp
+++ Tanque.cpp
@@ -5,13 +5,67 @@
 
 Tanque::Tanque(int n, int m, float dificultad)
 {
-	capacidadTotal = n*m * dificultad;
-	capacidadOcupada = 0;
-	capacidadLibre = capacidadTotal;
-	aguaDisponiblePorEsteTurno = 0;
+	this->capacidadTotal = n*m*dificultad;
+	this->capacidadOcupada = 0;
+	this->capacidadLibre = this->capacidadTotal;
+	this->aguaDisponiblePorEsteTurno = 0;
 }
 
 int Tanque::obtenerCapacidadTotal()
 {
 	return capacidadTotal;
+};
+
+int Tanque::obtenerCapacidadLibre()
+{
+	return this->capacidadLibre;
+};
+
+int Tanque::obtenerCapacidadOcupada()
+{
+	return this->capacidadOcupada;
+};
+
+
+int Tanque::determinarAguaPorEsteTurno()
+{
+	int dado = (rand() % 6 )+1;
+	int aguaAleatoria = (5*dado);
+	this->aguaDisponiblePorEsteTurno = aguaAleatoria + this->capacidadOcupada;
+	aguaAleatoria = 0;
+	return this-> aguaDisponiblePorEsteTurno;
+};
+
+int Tanque::obtenerAguaDisponible()
+{
+	return this->aguaDisponiblePorEsteTurno;
+}
+
+int Tanque::utilizarAgua(Lista<Semilla*>* semillas, char nombreSemillaSeleccionada)
+{	int aguaUtilizada = 0;
+	CatalogoSemillas catalogo;
+	Semilla* semillaDeCatalogo = catalogo.obtenerInformacionSemilla(semillas, nombreSemillaSeleccionada);
+
+	int cantidadDeAguaParaRegar = semillaDeCatalogo->obtenerConsumoAgua();
+
+	if(this->aguaDisponiblePorEsteTurno >= cantidadDeAguaParaRegar)
+	{
+		this->aguaDisponiblePorEsteTurno = this->aguaDisponiblePorEsteTurno - cantidadDeAguaParaRegar;
+		aguaUtilizada = cantidadDeAguaParaRegar;
+	}
+	return aguaUtilizada;
+};
+
+void Tanque::actualizarCapacidades()
+{
+	if(this->aguaDisponiblePorEsteTurno >= this->capacidadTotal)
+	{
+		this->capacidadLibre = 0;
+		this->capacidadOcupada = this->capacidadTotal;
+	}
+	else
+	{	this->capacidadOcupada = this->aguaDisponiblePorEsteTurno;
+		this->capacidadLibre = this->capacidadTotal - this->capacidadOcupada;
+	}
+	this->aguaDisponiblePorEsteTurno = this->capacidadOcupada;
 };
