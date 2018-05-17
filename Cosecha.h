cfceedde55d136001c6a162e72aa996d4dad0ff5
dleloutre <39065230+dleloutre@users.noreply.github.com>
2018-05-17 13:09:28 -0300

    Update Cosecha.h

--- Cosecha.h
+++ Cosecha.h
@@ -6,13 +6,26 @@ private:
 
 	char tipoCosecha;
 	int cantidadDeLaCosecha;
+
 public:
+
+	//post: asigna qué cultivo ha sido cosechado y qué cantidad del mismo
 	Cosecha(char tipoCosecha, int cantidadDeLaCosecha);
 
+	//post: devuelve la cantidad de cosecha
 	int obtenerCantidad();
+
+	//post: devuelve el cultivo que ha sido cosechado
 	char obtenerTipoCosecha();
+
+	//post: agrega una unidad de cosecha a 'cantidadDeLaCosecha'
 	void agregarUnaUnidadDeCosecha();
 };
+
+
+
+#endif /* COSECHA_H_ */
+
 
 
 
