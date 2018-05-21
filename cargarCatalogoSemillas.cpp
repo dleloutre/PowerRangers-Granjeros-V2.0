34a57d7b5fbc43625339012941b5e5bc26a206b6
dleloutre <39065230+dleloutre@users.noreply.github.com>
2018-05-21 19:04:32 -0300

    Update cargarCatalogoSemillas.cpp

--- cargarCatalogoSemillas.cpp
+++ cargarCatalogoSemillas.cpp
@@ -3,107 +3,86 @@
 #include "cargarCatalogoSemillas.h"
 
 
-CargaCatalogoSemillas::CargaCatalogoSemillas(Lista<Semilla*>* semillas){
+CargarCatalogoSemillas::CargarCatalogoSemillas(){
 
 	this->semillas = semillas;
 
 }
 
 //void leer archivo, solo lee y delega la tarea a otro
-void CargaCatalogoSemillas::leerArchivo(std::string rutaEntrada) {
+void CargarCatalogoSemillas::leerArchivo(std::string rutaEntrada) {
 
 	/* crea el archivo y abre la ruta especificada */
 	std::ifstream entrada;
 	entrada.open(rutaEntrada.c_str());
 
 	//declara un string
-	std::string stringLinea;
+	std::string getLinea;
 
 	/* lee el resto del archivo */
 	while (! entrada.eof()) {
 
-		std::getline(entrada, stringLinea);
+		std::getline(entrada, getLinea);
 
 		//clase funcion que haga todos lo de bajo
 
-		this -> cargarStringAVector(stringLinea);
+		this->cargarStringASemilla(getLinea);
 
 	} // while
 	/* cierra el archivo, liberando el recurso */
 	entrada.close();
 }
 
-void CargaCatalogoSemillas::cargarStringAVector(std::string stringLinea){
+void CargarCatalogoSemillas::cargarStringASemilla(std::string getLinea){
 
 	int i=0;
+
+	Traductor traductor;
 
 	//crea un vector para almacenar cada dato del txt.
 	std::string vectorDeStrings[CANTIDAD_DATOS_SEMILLAS];
 
-	//guarda el primer dato (A,B,C etc)
-	char semilla = stringLinea[i];
-
-	i+=2; //saltea la primera letra y la coma
-
-	//carga el vector con los datos de la linea, sin incluir la primera letra
+	//carga el vector con los datos de la linea
 	for (int j=0; j<= (CANTIDAD_DATOS_SEMILLAS-1); j++){ //
-		while(stringLinea[i] != ',' && stringLinea[i]!= '\0'){
-			vectorDeStrings[j] =  vectorDeStrings[j] + stringLinea[i];
+		while(getLinea[i] != ',' && getLinea[i]!= '\0'){
+			vectorDeStrings[j] =  vectorDeStrings[j] + getLinea[i];
 			i++;
 		}
 		i++;
 	}
 
-	int vectorSemillas[CANTIDAD_DATOS_SEMILLAS];
+	Semilla* punteroSemilla;
 
-	this->convertirVectorAEnteros(vectorSemillas, vectorDeStrings);
+	std::string nombre = vectorDeStrings[0];
 
-	this->agregarDatosVectorComoPunteroALaLista(vectorSemillas, semilla);
-}
+	int costo = traductor.convertirAEntero(vectorDeStrings[1]);
 
-void CargaCatalogoSemillas::convertirVectorAEnteros(int vectorSemillas[], std::string vectorDeStrings[]){
-	/*crea un vectorCultivos de enteros y le pasa los strings del
-	vectorDeStrings como int
-	*/
+	int tiempoCrecimiento = traductor.convertirAEntero(vectorDeStrings[2]);
 
-	int i,valor;
+	int rentabilidad = traductor.convertirAEntero(vectorDeStrings[3]);
 
-	// cambia los string a enteros
-	for (i=0; i<CANTIDAD_DATOS_SEMILLAS; i++){
-		std::istringstream buffer(vectorDeStrings[i]);
-		valor=0; //si ocurre algo, por defecto pone un 0
-		buffer >> valor;
-		vectorSemillas[i]=valor;
-	}
-}
+	int tiempoRecuperacion = traductor.convertirAEntero(vectorDeStrings[4]);
 
-// ********** funcion asignar puntero (vectorcultivos puntero a vectorcultivos
-
-void CargaCatalogoSemillas::agregarDatosVectorComoPunteroALaLista(int vectorSemillas[], char semilla){
-
-	Semilla* ptrSemilla;
-
-	char nombre = semilla;
-
-	int costo = vectorSemillas[0];
-
-	int tiempoCrecimiento = vectorSemillas[1];
-
-	int rentabilidad = vectorSemillas[2];
-
-	int tiempoRecuperacion = vectorSemillas[3];
-
-	int consumoAgua = vectorSemillas[4];
+	int consumoAgua = traductor.convertirAEntero(vectorDeStrings[5]);
 
 	/*asigno un espacio en el heap para un puntero a una Semilla*/
-	ptrSemilla = new Semilla(nombre, costo, tiempoCrecimiento,
+	punteroSemilla = new Semilla(nombre, costo, tiempoCrecimiento,
 			 rentabilidad, tiempoRecuperacion, consumoAgua);
 
-	this->semillas->agregar(ptrSemilla);
+	this->semillas->agregar(punteroSemilla);
+
+
+	return;
 }
 
-CargaCatalogoSemillas::~CargaCatalogoSemillas(){
+Lista<Semilla*>* CargarCatalogoSemillas::obtenerPunteroAListaDeSemillas(){
 
+	return this ->semillas;
+}
+
+
+
+CargarCatalogoSemillas::~CargarCatalogoSemillas(){
 
 	semillas->iniciarCursor();
 
@@ -113,4 +92,7 @@ CargaCatalogoSemillas::~CargaCatalogoSemillas(){
 		delete PtrABorrar;
 	}
 
+	delete this->semillas;
+
 }
+
