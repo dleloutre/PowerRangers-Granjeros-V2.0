a6f24c1bc6aac99735209c8b31b190c8b30cf610
dleloutre <39065230+dleloutre@users.noreply.github.com>
2018-05-21 19:03:00 -0300

    Update Terreno.h

--- Terreno.h
+++ Terreno.h
@@ -1,35 +1,36 @@
 #ifndef TERRENO_H_
 #define TERRENO_H_
-const int TAMANIO_MINIMO = 2;
 
 #include "Parcela.h"
 
 class Terreno {
 
 	private:
-		unsigned int tamanio;
+		unsigned int filas;
+		unsigned int columnas;
 		Parcela** terreno;
 		Parcela* accesoATerreno;
 
 	public:
 
-		//POST: crea la matriz 'terreno' de TAMANIO_MINIMO filas y TAMANIO_MINIMO columnas
-		Terreno();
-
-		//POST: crea la matriz 'terreno' de tamanio filas y tamanio columnas
-		Terreno(unsigned int tamanio);
+		//POST: crea la matriz 'terreno' de tamanio filasxcolumnas
+		Terreno(unsigned int filas, unsigned int columnas);
 
 		//POST: devuelve la parcela que se encuentra en la posicionFila y posicionColumna del terreno
 		Parcela obtenerParcela(unsigned int posicionFila, unsigned int posicionColumna);
 
 		//POST: accede a la parcela que se encuentra en la posicionFila y posicionColumna del terreno
-		Parcela* modificarTerreno(unsigned int posicionFila, unsigned int posicionColumna);
+		Parcela* modificarParcela(unsigned int posicionFila, unsigned int posicionColumna);
 
-		//POST: devuelve la cantidad de filas y columnas de la matriz 'terreno'
-		unsigned int obtenerTamanio();
+		//POST: devuelve la cantidad de filas de la matriz 'terreno'
+		unsigned int obtenerFilas();
+
+		//POST: devuelve la cantidad de columnas de la matriz 'terreno'
+		unsigned int obtenerColumnas();
 
 		~Terreno();
 };
+
 
 
 #endif /* TERRENO_H_ */
