4172bd1102a8e20fce151db1dfa3d8c765ab02c8
dleloutre <39065230+dleloutre@users.noreply.github.com>
2018-06-28 07:21:41 -0300

    Create BMPTerreno.h

---
+++ BMPTerreno.h
@@ -0,0 +1,29 @@
+#ifndef BMPTERRENO_H_
+#define BMPTERRENO_H_
+
+#include "EasyBMP.h"
+#include "Terreno.h"
+#include "Traductor.h"
+
+
+const float DEFAULT = 5;  // Dividendo base para obtener
+	 	 	 	 	 	//  el RATIO W y RATIOH
+
+const float TAMANIO_BMP_PARCELA= 50; //Tamaño del BMP de los cultivos 100 x 100
+
+const float DISTANCIA_ENTRE_PARCELAS = 100;
+
+const float DISTANCIA_BORDE_IMAGEN = 25;
+
+
+class BMPTerreno {
+
+	public:
+
+	//post: crea imagen en BMP del terreno
+	void guardarTerrenoEnBMP(int argc, char* argv[], Terreno* Terreno, std::string jugador, int numeroDeTerreno, int numeroDeTurno);
+
+};
+
+
+#endif /* BMPTERRENO_H_ */
