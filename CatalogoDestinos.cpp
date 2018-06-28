3c09ce3a6357d621b17a8b7156b7e2008006bcc1
dleloutre <39065230+dleloutre@users.noreply.github.com>
2018-06-28 07:22:42 -0300

    Create CatalogoDestinos.cpp

---
+++ CatalogoDestinos.cpp
@@ -0,0 +1,37 @@
+#include "CatalogoDestinos.h"
+
+Destino* CatalogoDestinos::obtenerDestino(Lista<Destino*>* destinos, std::string cultivo){
+
+	Destino* ptrClaseDestino=NULL;
+	bool encontro=false;
+
+	destinos->iniciarCursor();
+
+	while(destinos->avanzarCursor() && !encontro){
+		if(destinos->obtenerCursor()->obtenerCultivo() == cultivo){
+			ptrClaseDestino = destinos ->obtenerCursor();
+			encontro = true;
+		}
+	}
+
+	return ptrClaseDestino;
+
+}
+
+Destino* CatalogoDestinos::obtenerDestino(Lista <Destino*>* destinos, unsigned int posicion) {
+	Destino* destino = NULL;
+	unsigned int posicionLista = 1;
+
+	destinos->iniciarCursor();
+
+	while(destinos->avanzarCursor()){
+		Destino* destinoActual = destinos->obtenerCursor();
+
+		if(posicionLista == posicion){
+			destino = destinoActual;
+		}
+		posicionLista++;
+	}
+	return destino;
+}
+
