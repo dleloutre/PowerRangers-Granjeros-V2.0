18fa9c8e925360c447d034d12713ab5580ba613b
dleloutre <39065230+dleloutre@users.noreply.github.com>
2018-06-28 07:23:32 -0300

    Update CatalogoSemillas.cpp

--- CatalogoSemillas.cpp
+++ CatalogoSemillas.cpp
@@ -10,10 +10,26 @@ Semilla* CatalogoSemillas::obtenerInformacionSemilla(Lista<Semilla*>* semillas, 
 	while(semillas->avanzarCursor() && !semillaEncontrada){
 		semilla = semillas->obtenerCursor();
 
-		if(semillas->obtenerCursor()->obtenerNombre() == nombreSemillaSeleccionada){
-					semilla = semillas->obtenerCursor() ;
+		if(semilla->obtenerNombre() == nombreSemillaSeleccionada){
 					semillaEncontrada = true;
 				}
+	}
+	return semilla;
+}
+
+Semilla* CatalogoSemillas::obtenerInformacionSemilla(Lista <Semilla*>* semillas, unsigned int posicion) {
+	Semilla* semilla = NULL;
+	unsigned int posicionLista = 1;
+
+	semillas->iniciarCursor();
+
+	while(semillas->avanzarCursor()){
+		Semilla* semillaActual = semillas->obtenerCursor();
+
+		if(posicionLista == posicion){
+			semilla = semillaActual;
+		}
+		posicionLista++;
 	}
 	return semilla;
 }
